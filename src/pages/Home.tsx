7a35dba74a0805ff429a3f78f4e2fe1438b38c83
sachin shakya <sachin@firstapp.io>
2023-12-23 15:03:38 +0530

    update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,7 +56,7 @@ function Home() {
       "bg-[#781E1E] text-[white!importanat] duration-700 ease-in-out text-white"
     }
     ${theme === 3 && "bg-[#DAB0A4] duration-700 ease-in-out"}
-     px-2 md:px-20 py-10 h-full lg:h-screen
+     px-2 md:px-20 py-10 h-full 2xl:h-screen
 
      
   `}
