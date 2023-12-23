12ff09b04c80316908e85255de79cbebd7f56a58
sachin shakya <sachin@firstapp.io>
2023-12-23 08:12:44 +0530

    impliment react-hook-form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 function Register() {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onsubmit = handleSubmit(async (data) => {
+    console.log(data);
+  });
+
   return (
     <div>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -19,25 +33,7 @@ function Register() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  autoComplete="username"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+          <div className="space-y-6">
             <div>
               <label
                 htmlFor="email"
@@ -48,6 +44,7 @@ function Register() {
               <div className="mt-2">
                 <input
                   id="email"
+                  {...register("email")}
                   name="email"
                   type="email"
                   autoComplete="email"
@@ -65,18 +62,11 @@ function Register() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
+                  {...register("password")}
                   name="password"
                   type="password"
                   autoComplete="current-password"
@@ -87,14 +77,36 @@ function Register() {
             </div>
 
             <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="ConfirmPassword"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  {...register("confirmPassword")}
+                  name="confirmPassword"
+                  type="password"
+                  autoComplete="confirm-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
               <button
-                type="submit"
+                onClick={() => onsubmit()}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-          </form>
+          </div>
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Have a Account?{" "}
